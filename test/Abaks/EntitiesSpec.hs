90a27a7e018bacdae598dbfe2579f5bcf95148bc
Gautier DI FOLCO <gautier.difolco@gmail.com>
2023-06-15 20:03:01 +0200

    feature(api): start testing core domain

--- test/Abaks/EntitiesSpec.hs
+++ test/Abaks/EntitiesSpec.hs
@@ -5,6 +5,11 @@ module Abaks.EntitiesSpec
 where
 
 import Abaks.Entities
+import Data.Either
+import Data.Functor.Classes
+import Data.List (foldl')
+import Data.Text (Text)
+import Data.Time.Calendar
 import Test.Hspec
 
 main :: IO ()
@@ -12,13 +17,31 @@ main = hspec spec
 
 spec :: Spec
 spec = do
-  return ()
+  describe "Entities" $ do
+    it "startPeriod should work" $
+      applyCommands [startPeriod anyPeriodId periodName periodStart periodEnd (Amount 0)]
+        `shouldSatisfy` liftEq ($) [isRight]
+    it "deleteEntry should fail" $
+      applyCommands [deleteEntry (EntryId 42) "do not exist"]
+        `shouldSatisfy` liftEq ($) [isLeft]
 
 applyCommands ::
   [CommandHandler AbaksEvent ExplainedError] ->
   [Either ExplainedError (Events AbaksEvent)]
-applyCommands = map snd . scanl go (mempty, Left "")
+applyCommands = snd . foldl' go (mempty, mempty)
   where
-    go (events, _) handler =
+    go (events, previousResults) handler =
       let result = applyCommand handler events
-       in (either (const events) (events <>) result, result)
+       in (either (const events) (events <>) result, previousResults <> [result])
+
+anyPeriodId :: PeriodId
+anyPeriodId = PeriodId 0
+
+periodName :: Text
+periodName = "Jan 2023"
+
+periodStart :: Day
+periodStart = fromGregorian 2023 1 1
+
+periodEnd :: Day
+periodEnd = fromGregorian 2023 1 31
