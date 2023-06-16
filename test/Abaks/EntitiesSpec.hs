7390f526d177e00d45a2efa956beb604ae4e6542
Gautier DI FOLCO <gautier.difolco@gmail.com>
2023-06-16 19:21:37 +0200

    feature(api): properly testing core domain

--- test/Abaks/EntitiesSpec.hs
+++ test/Abaks/EntitiesSpec.hs
@@ -19,11 +19,69 @@ spec :: Spec
 spec = do
   describe "Entities" $ do
     it "startPeriod should work" $
-      applyCommands [startPeriod anyPeriodId periodName periodStart periodEnd (Amount 0)]
+      applyCommands [startingCommand]
         `shouldSatisfy` liftEq ($) [isRight]
-    it "deleteEntry should fail" $
-      applyCommands [deleteEntry (EntryId 42) "do not exist"]
-        `shouldSatisfy` liftEq ($) [isLeft]
+    it "Not started should fail" $
+      applyCommands
+        [ addEntry anEntry,
+          changeAmountEntry anEntry.entryId (Amount 1500),
+          validateEntry anEntry.entryId,
+          commentEntry anEntry.entryId "The Hateful 8: too cool",
+          markInClonflictEntry anEntry.entryId "way too expensive",
+          deleteEntry anEntry.entryId "wrong period"
+        ]
+        `shouldSatisfy` liftEq ($) [isLeft, isLeft, isLeft, isLeft, isLeft, isLeft]
+    it "Out of period should fail" $
+      applyCommands
+        [ startingCommand,
+          addEntry anEntry {date = fromGregorian 2022 1 15},
+          addEntry anEntry {date = fromGregorian 2024 1 15}
+        ]
+        `shouldSatisfy` liftEq ($) [isRight, isLeft, isLeft]
+    it "All chained commands should work" $
+      applyCommands
+        [ startingCommand,
+          addEntry anEntry,
+          changeAmountEntry anEntry.entryId (Amount 1500),
+          validateEntry anEntry.entryId,
+          commentEntry anEntry.entryId "The Hateful 8: too cool",
+          markInClonflictEntry anEntry.entryId "way too expensive",
+          deleteEntry anEntry.entryId "wrong period"
+        ]
+        `shouldSatisfy` liftEq ($) [isRight, isRight, isRight, isRight, isRight, isRight, isRight]
+    it "Wrong entryId should fail" $
+      applyCommands
+        [ startingCommand,
+          addEntry anEntry {entryId = EntryId 42},
+          changeAmountEntry anEntry.entryId (Amount 1500),
+          validateEntry anEntry.entryId,
+          commentEntry anEntry.entryId "The Hateful 8: too cool",
+          markInClonflictEntry anEntry.entryId "way too expensive",
+          deleteEntry anEntry.entryId "wrong period"
+        ]
+        `shouldSatisfy` liftEq ($) [isRight, isRight, isLeft, isLeft, isLeft, isLeft, isLeft]
+    it "No entry should fail" $
+      applyCommands
+        [ startingCommand,
+          changeAmountEntry anEntry.entryId (Amount 1500),
+          validateEntry anEntry.entryId,
+          commentEntry anEntry.entryId "The Hateful 8: too cool",
+          markInClonflictEntry anEntry.entryId "way too expensive",
+          deleteEntry anEntry.entryId "wrong period"
+        ]
+        `shouldSatisfy` liftEq ($) [isRight, isLeft, isLeft, isLeft, isLeft, isLeft]
+    it "Deleted entry should fail" $
+      applyCommands
+        [ startingCommand,
+          addEntry anEntry,
+          deleteEntry anEntry.entryId "wrong period",
+          changeAmountEntry anEntry.entryId (Amount 1500),
+          validateEntry anEntry.entryId,
+          commentEntry anEntry.entryId "The Hateful 8: too cool",
+          markInClonflictEntry anEntry.entryId "way too expensive",
+          deleteEntry anEntry.entryId "wrong period"
+        ]
+        `shouldSatisfy` liftEq ($) [isRight, isRight, isRight, isLeft, isLeft, isLeft, isLeft, isLeft]
 
 applyCommands ::
   [CommandHandler AbaksEvent ExplainedError] ->
@@ -45,3 +103,17 @@ periodStart = fromGregorian 2023 1 1
 
 periodEnd :: Day
 periodEnd = fromGregorian 2023 1 31
+
+anEntry :: Entry
+anEntry =
+  Entry
+    { entryId = EntryId 1,
+      amount = Amount 2000,
+      category = "cinema",
+      comment = "The Hateful 8",
+      state = Unexpected,
+      date = fromGregorian 2023 1 24
+    }
+
+startingCommand :: CommandHandler AbaksEvent ExplainedError
+startingCommand = startPeriod anyPeriodId periodName periodStart periodEnd (Amount 0)
